3d298b771343278ed127085c14bdca7e1c818c9c
jlcampbell1991@gmail.com <jlcampbell1991@gmail.com>
2020-05-20 12:50:34 -0500

    Add remaining CRUD routes

--- src/main/g8/$app_name$/src/main/scala/$package__packaged$/routes/$class_name__Camel$Routes.scala
+++ src/main/g8/$app_name$/src/main/scala/$package__packaged$/routes/$class_name__Camel$Routes.scala
@@ -15,15 +15,33 @@ object $class_name;format="Camel"$Routes extends Routes {
     import dsl._
     authedService(
       (userId: UserId) => HttpRoutes.of {
+        case GET -> Root / "$class_name;format="normalize"$"s => {
+          $class_name;format="camel"$s <- $class_name;format="Camel"$.all
+          response <- $class_name;format="Camel"$.index($class_name;format="camel"$s)
+        } yield response
         case GET -> Root / "$class_name;format="normalize"$" / id => for {
           $class_name;format="camel"$ <- $class_name;format="Camel"$.find($class_name;format="Camel"$Id(id))
           response <- $class_name;format="camel"$.show
         } yield Ok(response)
-        case GET -> Root / "$class_name;format="normalize"$" / "new"
-        case req @ POST -> Root / "$class_name;format="normalize"$" / "create"
-        case GET -> Root / "$class_name;format="normalize"$" / id / "edit"
-        case req @ PUT  -> Root / "$class_name;format="normalize"$" / id / "update"
-        case DELETE -> Root / "$class_name;format="normalize"$" / id
+        case GET -> Root / "$class_name;format="normalize"$" / "add" => Ok($class_name;format="Camel"$.add)
+        case req @ POST -> Root / "$class_name;format="normalize"$" / "create" => for {
+          form <- req.as[UrlForm]
+          $class_name;format="camel"$ <- $class_name;format="Camel"$.fromUrlForm(form).map(_.save)
+          response <- Redirect($class_name;format="camel"$.showUrl)
+        } yield response
+        case GET -> Root / "$class_name;format="normalize"$" / id / "edit" => for {
+          $class_name;format="camel"$ <- $class_name;format="Camel"$.find($class_name;format="Camel"$Id(id))
+          response <- Ok($class_name;format="camel"$.edit)
+        } yield response
+        case req @ PUT  -> Root / "$class_name;format="normalize"$" / id / "update" => for {
+          form <- req.as[UrlForm]
+          $class_name;format="camel"$ <- $class_name;format="Camel"$.fromUrlForm(form).map(_.update)
+          response <- Redirect($class_name;format="camel"$.showUrl)
+        } yield response
+        case DELETE -> Root / "$class_name;format="normalize"$" / id for {
+          _ <- $class_name;format="Camel"$.destroy($class_name;format="Camel"$Id(id))
+          response <- Redirect($class_name;format="Camel"$.indexUrl)
+        } yield response
       }
     )
   }
