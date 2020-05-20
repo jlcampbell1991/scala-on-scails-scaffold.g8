4a439ba186bbca26cb2ac079f40d57ecfc219d8b
jlcampbell1991@gmail.com <jlcampbell1991@gmail.com>
2020-05-20 12:17:47 -0500

    Correct table_columns

--- src/main/g8/$app_name$/src/main/scala/$package__packaged$/db/$class_name__Camel$Table.scala
+++ src/main/g8/$app_name$/src/main/scala/$package__packaged$/db/$class_name__Camel$Table.scala
@@ -2,12 +2,12 @@ package $package$
 
 object $class_name;format="Camel"$Table extends Table {
   def initialize: Update0 = sql"""
-    DROP TABLE IF EXISTS $class_name;format="snake"$;
-    CREATE TABLE $class_name;format="snake"$(
+    DROP TABLE IF EXISTS $class_name;format="snake, lower"$;
+    CREATE TABLE $class_name;format="snake, lower"$(
       $table_columns$
     )""".update
 
   def update: Update0 =
-    sql"""DROP TABLE IF EXISTS $class_name;format="snake"$"""
+    sql"""DROP TABLE IF EXISTS $class_name;format="snake, lower"$"""
     .update
 }
