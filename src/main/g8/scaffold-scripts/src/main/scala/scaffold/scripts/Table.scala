324ee980e39b0bbf04e0ca3500fbb90b1d97d3fb
jlcampbell1991@gmail.com <jlcampbell1991@gmail.com>
2020-06-05 09:53:02 -0500

    Correct path in Table

--- src/main/g8/scaffold-scripts/src/main/scala/scaffold/scripts/Table.scala
+++ src/main/g8/scaffold-scripts/src/main/scala/scaffold/scripts/Table.scala
@@ -3,7 +3,7 @@ package scaffold.scripts
 object Table extends ScaffoldWriter {
   def update = {
     replace(
-      s"./test-app/src/main/scala/test/app/db/\${CamelCase}Table.scala",
+      s"./$app_name$/src/main/scala/$path$/db/\${CamelCase}Table.scala",
       s"\${UPPER_SNAKE_CASE}_TABLE",
       Fields.toSql
     )
