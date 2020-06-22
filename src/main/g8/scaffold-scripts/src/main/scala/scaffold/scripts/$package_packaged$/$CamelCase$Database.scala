5db02c584c588e2918e6e4554ac5ae3db45a2e7d
jlcampbell1991@gmail.com <jlcampbell1991@gmail.com>
2020-06-22 16:08:57 -0500

    Rearrange scaffold-scripts

--- src/main/g8/scaffold-scripts/src/main/scala/scaffold/scripts/$package_packaged$/$CamelCase$Database.scala
+++
@@ -1,9 +0,0 @@
-package scaffold.scripts
-
-object $CamelCase$Database extends ScaffoldWriter {
-  def update = append(
-    "./$app_name$/src/main/scala/$path$/db/Database.scala",
-    "List(UserTable)",
-    s""" ::: List(\${CamelCase}Table)"""
-  )
-}
